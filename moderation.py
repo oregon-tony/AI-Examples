627b322d33af40be66a932ba8f942b1a3feb6db2
oregon_tony <oregon_tony@yahoo.com>
2024-05-19 20:51:16 -0700

    Example of Moderation API call

---
+++ moderation.py
@@ -0,0 +1,29 @@
+# Python Script to use Moderation Classifications 
+import json
+import os
+from pprint import pprint
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from openai import OpenAI
+# Utility Functions
+oai_client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+response = oai_client.moderations.create(input="I'm going to hit myself on the head.")
+
+if response.results[0].flagged:
+    print("Threats Detected")
+    print(response)
+
+    for result in response.results:
+        categories = result.categories
+        pprint(categories.__dict__)
+
+else:
+    print("No Threats Detected")
+
+
