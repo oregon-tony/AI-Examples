751b30bf729a9f34a1a958a6047b0934c5b6e213
oregon_tony <oregon_tony@yahoo.com>
2024-05-22 20:50:46 -0700

    Classify text as being neutral, positive, or negative.

--- sentiment.py
+++ sentiment.py
@@ -3,7 +3,7 @@
 # Take a string of sentence(s) and classify each one as positive or negative.
 # This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
 # If you have an Azure account, you can create a 'free' resource to run this example, see URL ^^
-
+ 
 import os
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
